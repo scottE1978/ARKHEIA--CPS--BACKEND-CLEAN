a52b1b949cd717e664fd961455f3890208e1dcfb
scottE1978 <ypk9vpbb7m@privaterelay.appleid.com>
2026-07-16 01:13:45 -0400

    Update main.py

--- arkheia_cps/app/main.py
+++ arkheia_cps/app/main.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
+from arkheia_cps.routers.billing import router as billing_router
+from arkheia_cps.routers.contracts import router as contracts_router
 
+app = FastAPI()
+
+app.include_router(billing_router)
+app.include_router(contracts_router)
