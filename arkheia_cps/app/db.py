47a0a40f05abd410b739ca62bcc1eea6311f8d02
scottE1978 <ypk9vpbb7m@privaterelay.appleid.com>
2026-07-17 11:18:07 -0400

    Update db.py

--- arkheia_cps/app/db.py
+++ arkheia_cps/app/db.py
@@ -1,18 +1,12 @@
+print("LOADING DB MODULE")
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./arkheia.db"
+from app.config import settings
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
